b085e4dbf645a00bc42045462723229f984a3443
dip parmar <dipparmar4637@gmail.com>
2025-02-02 12:36:09 +0530

    multiplayer game next level

--- Tic Tac Toe/online_play/Client.py
+++ Tic Tac Toe/online_play/Client.py
@@ -1,8 +1,38 @@
-# gameboard.py
+import socket
+import pickle
+from gameboard import GameBoard
 
-class GameBoard:
-    def __init__(self, board_state):
-        self.board_state = board_state
+def client_program():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("127.0.0.1", 9999))
 
-    def update_board(self, new_state):
-        self.board_state = new_state
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+
+        message = pickle.loads(data)
+        if isinstance(message, str):
+            print(message)
+            if message == "Only one client connected":
+                # Handle the situation where only one client is connected
+                pass
+            elif message == "Two clients connected":
+                # Handle the situation where both clients are connected
+                game_board = GameBoard("client initial state")
+                data_to_send = pickle.dumps(game_board)
+                client_socket.sendall(data_to_send)
+        else:
+            received_board = message
+            print(f"Received from server: {received_board.board_state}")
+
+            # Update game board or send a new one
+            new_state = "updated state from client"
+            game_board.update_board(new_state)
+            data_to_send = pickle.dumps(game_board)
+            client_socket.sendall(data_to_send)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    client_program()
--- Tic Tac Toe/online_play/gameboard.py
+++ Tic Tac Toe/online_play/gameboard.py
@@ -1,45 +1,9 @@
-import socket
-import threading
-import pickle
-
 class GameBoard:
-    def __init__(self, board_state):
-        self.board_state = board_state
+    def __init__(self, sign, state):
+        self.sign = sign
+        self.game_board = {(i, j): None for i in range(3) for j in range(3)}
+        self.state = state
 
-    def update_board(self, new_state):
-        self.board_state = new_state
-
-def handle_client(client_socket, game_board):
-    while True:
-        try:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-
-            received_board = pickle.loads(data)
-            print(f"Received: {received_board.board_state}")
-
-            game_board.update_board(received_board.board_state)
-            data_to_send = pickle.dumps(game_board)
-            client_socket.sendall(data_to_send)
-        except:
-            break
-
-    client_socket.close()
-
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("0.0.0.0", 9999))
-    server.listen(5)
-    print("Server listening on port 9999")
-
-    game_board = GameBoard("initial state")
-    while True:
-        client_socket, addr = server.accept()
-        print(f"Accepted connection from {addr}")
-
-        client_handler = threading.Thread(target=handle_client, args=(client_socket, game_board))
-        client_handler.start()
-
-if __name__ == "__main__":
-    start_server()
+    def update_board(self, board, state):
+        self.game_board = board
+        self.state = state
